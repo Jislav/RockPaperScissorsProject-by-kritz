62bc00a8660a13710d5c45c054d907a3883be107
kritz <jislavjj@gmail.com>
2024-01-24 16:08:57 +0200

    Added new project "GuessNumber"

---
+++ GuessNumber/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace GuessNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Guess a number (1-100):");
+
+            Random randomNumber = new Random();
+            int winningNumber = randomNumber.Next(1, 101);
+
+            int guess = int.Parse(Console.ReadLine());
+
+            while (guess != winningNumber)
+            {
+                if (guess > winningNumber)
+                {
+                    Console.WriteLine("Too high");
+                }
+                else
+                {
+                    Console.WriteLine("Too low");
+                }
+                Console.Write("Guess a number (1-100):");
+                guess = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("You guessed it!");
+        }
+    }
+}
